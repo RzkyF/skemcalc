1e3a69cd6f6d6a1528b8711fcfffe2ebaf2c71e1
rizki.fadillah <rizki.fadillah@konekthing.com>
2025-11-07 23:47:56 +0700

    Ver 1

---
+++ Public/js/config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "primary": "#015592",
+          "secondary": "#f9d309",
+          "text-primary": "#333333",
+          "border-color": "#E0E0E0",
+        },
+        fontFamily: {
+          "display": ["Manrope", "sans-serif"]
+        },
+        borderRadius: {"DEFAULT": "0.5rem", "lg": "0.75rem", "xl": "1rem", "full": "9999px"},
+      },
+    },
+  }
