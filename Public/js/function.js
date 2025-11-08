00a9a375fd691637dbdf9d8f554e14b5ed35aa76
rizki.fadillah <rizki.fadillah@konekthing.com>
2025-11-08 08:31:23 +0700

    test rice cooker

--- Public/js/function.js
+++ Public/js/function.js
@@ -1,9 +1,29 @@
 //input range 
-const rangeDays = document.getElementById("rangeDays");
-const spanDays = rangeDays.parentElement.querySelector("span");
+const jumlahPenggunaan = document.getElementById("jumlahPenggunaan");
+const spanDays = jumlahPenggunaan.parentElement.querySelector("span");
 
-rangeDays.addEventListener("input", () => {
-  spanDays.textContent = `${rangeDays.value} hari`;
+jumlahPenggunaan.addEventListener("input", () => {
+  spanDays.textContent = `${jumlahPenggunaan.value} hari`;
 });
 
-//
+//perhitungan
+       document.getElementById('btnHitung').addEventListener('click', function() {
+            const DayaMemasak = parseFloat(document.getElementById('DayaMemasak').value) || 0;
+            const DayaMenghangatkan = parseFloat(document.getElementById('DayaMenghangatkan').value) || 0;
+            const SiklusMemasak = parseFloat(document.getElementById('SiklusMemasak').value) || 0;
+            const SiklusMenghangatkan = parseFloat(document.getElementById('SiklusMenghangatkan').value) || 0;
+            const jumlahPenggunaan = parseFloat(document.getElementById('jumlahPenggunaan').value) || 0;
+            const kwhPenanakNasi = (DayaMemasak * SiklusMemasak + DayaMenghangatkan * SiklusMenghangatkan * jumlahPenggunaan) / 1000;
+
+        if (DayaMemasak, DayaMemasak, SiklusMemasak, SiklusMenghangatkan, jumlahPenggunaan <= 0) {
+            const hasilHitung = document.getElementById('hasilHitung');
+            hasilHitung.innerHTML = `Input daya tidak boleh 0`;
+            hasilHitung.style.display = 'block';
+
+        } else {
+            const hasilHitung = document.getElementById('hasilHitung');
+            hasilHitung.innerHTML = kwhPenanakNasi;
+            hasilHitung.style.display = 'block';
+        }
+    
+    });
